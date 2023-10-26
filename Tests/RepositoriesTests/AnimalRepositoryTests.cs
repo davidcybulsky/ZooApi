363288c2063f9f97c6b37f78661e0b2a1997294f
sekulas <sekula.sebastian22@gmail.com>
2023-10-26 22:59:15 +0200

    Animal repository tests added

---
+++ Tests/RepositoriesTests/AnimalRepositoryTests.cs
@@ -0,0 +1,90 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Zoo.Entities;
+using ZooApi.Data;
+using ZooApi.Repositories;
+
+namespace Tests.RepositoriesTests;
+public class AnimalRepositoryTests
+{
+    private readonly ZooContext _context;
+    private readonly AnimalRepository _animalRepository;
+
+    public AnimalRepositoryTests()
+    {
+        var options = new DbContextOptionsBuilder<ZooContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        _context = new ZooContext(options);
+        _animalRepository = new AnimalRepository(_context);
+    }
+
+    [Fact]
+    public void Create_ShouldAddAnimalToDatabase()
+    {
+        // Arrange
+        var animal = new Animal { Name = "TestAnimal", Species = Species.TIGER };
+
+        // Act
+        _animalRepository.Create(animal);
+
+        // Assert
+        var savedAnimal = _context.Animals.FirstOrDefault(a => a.Id == animal.Id);
+        savedAnimal.Should().NotBeNull();
+        savedAnimal.Name.Should().Be(animal.Name);
+        savedAnimal.Species.Should().Be(animal.Species);
+    }
+
+    [Fact]
+    public void Read_ShouldReturnAnimalFromDatabase()
+    {
+        // Arrange
+        var animal = new Animal { Name = "TestAnimal", Species = Species.LION };
+        _context.Animals.Add(animal);
+        _context.SaveChanges();
+
+        // Act
+        var retrievedAnimal = _animalRepository.Read(animal.Id);
+
+        // Assert
+        retrievedAnimal.Should().NotBeNull();
+        retrievedAnimal.Id.Should().Be(animal.Id);
+        retrievedAnimal.Name.Should().Be(animal.Name);
+        retrievedAnimal.Species.Should().Be(animal.Species);
+    }
+
+    [Fact]
+    public void Update_ShouldUpdateAnimalInDatabase()
+    {
+        // Arrange
+        var animal = new Animal { Name = "TestAnimal", Species = Species.PARROT };
+        _context.Animals.Add(animal);
+        _context.SaveChanges();
+        var updatedAnimal = new Animal { Name = "UpdatedAnimal", Species = Species.PANDA };
+
+        // Act
+        _animalRepository.Update(animal.Id, updatedAnimal);
+
+        // Assert
+        var retrievedAnimal = _context.Animals.FirstOrDefault(a => a.Id == animal.Id);
+        retrievedAnimal.Should().NotBeNull();
+        retrievedAnimal.Name.Should().Be(updatedAnimal.Name);
+        retrievedAnimal.Species.Should().Be(updatedAnimal.Species);
+    }
+
+    [Fact]
+    public void Delete_ShouldRemoveAnimalFromDatabase()
+    {
+        // Arrange
+        var animal = new Animal { Name = "TestAnimal", Species = Species.DOLPHIN };
+        _context.Animals.Add(animal);
+        _context.SaveChanges();
+
+        // Act
+        _animalRepository.Delete(animal.Id);
+
+        // Assert
+        var deletedAnimal = _context.Animals.FirstOrDefault(a => a.Id == animal.Id);
+        deletedAnimal.Should().BeNull();
+    }
+}
